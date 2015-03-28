4920c6356c8fe0568b7bbf59335fd5fa54521946
Kevin Gill <hkevgill@hotmail.com>
2015-03-28 13:02:34 -0700

    Query a should now be correct

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@ a)
 
 SELECT INCOMING.flightID AS f1, OUTGOING.flightID AS f2
 FROM INCOMING, OUTGOING
-WHERE plannedDepartureTime - plannedArrivalTime <= 3 AND plannedDepartureTime - plannedArrivalTime > 0
+WHERE (plannedDepartureTime - plannedArrivalTime <= 0.125) AND (plannedDepartureTime - plannedArrivalTime > 0)
 
 b)
 
