568005f8c3ce7f4d57d83edd1af9f74d9a305158
Kevin Gill <hkevgill@hotmail.com>
2015-03-29 22:08:34 -0700

    Fixed some queries

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -11,24 +11,20 @@ b)
 
 "SELECT flightID
 FROM FLIGHTS
-WHERE source = " + place + " OR destination = " + place + "
+WHERE source = " + location + " OR destination = " + location + "
 "
 
 c) 
 
-"SELECT * 
-FROM DEPARTURES UNION ALL ARRIVALS
-WHERE (" + timeOfDay + " - departureDate) < (1/24) OR (arrivalDate - " + timeOfDay + ") < (1/24)"
-
-SELECT *
-FROM
-  ((SELECT departureGate, departureDate, departureStatus
-  FROM DEPARTURES
-  WHERE (SYSDATE - departureDate) < (1/24) AND (SYSDATE - departureDate) > (-1/24))
-    NATURAL FULL OUTER JOIN
-  (SELECT arrivalGate, arrivalDate, arrivalStatus
-  FROM ARRIVALS
-  WHERE ((arrivalDate - SYSDATE) < (1/24) AND (arrivalDate - SYSDATE) > (-1/24))))
+"SELECT * " +
+"FROM " +
+  "((SELECT departureGate, departureDate, departureStatus AS Status " +
+  "FROM DEPARTURES " +
+  "WHERE (TO_DATE('" + timeOfDay + "', 'yyyy-mm-dd hh24:mi:ss') - departureDate) < (1/24) AND (TO_DATE('" + timeOfDay + "', 'yyyy-mm-dd hh24:mi:ss') - departureDate) > (-1/24)) " +
+    "NATURAL FULL OUTER JOIN " +
+  "(SELECT arrivalGate, arrivalDate, arrivalStatus AS Status " +
+  "FROM ARRIVALS " +
+  "WHERE ((arrivalDate - TO_DATE('" + timeOfDay + "', 'yyyy-mm-dd hh24:mi:ss')) < (1/24) AND (arrivalDate - TO_DATE('" + timeOfDay + "', 'yyyy-mm-dd hh24:mi:ss')) > (-1/24))))");
 
 d)
 
